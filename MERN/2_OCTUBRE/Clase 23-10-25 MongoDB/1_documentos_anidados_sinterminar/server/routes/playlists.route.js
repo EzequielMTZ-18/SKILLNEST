c1fcbb4f79a5b8eeafe27e2cd1fa746a5ea48664
Ezequiel Martinez <EzequielMartinezA18@gmail.com>
2025-10-30 19:37:33 -0300

    Limpieza
    Borrando basura

--- MERN/2_OCTUBRE/Clase 23-10-25 MongoDB/1_documentos_anidados_sinterminar/server/routes/playlists.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import playlistController from "../controllers/playlists.controller.js";
-
-const playlistsRoutes = Router();
-
-playlistsRoutes.get('/', playlistController.getAll)
-playlistsRoutes.post("/", playlistController.createPlaylist)
-
-export default playlistsRoutes
